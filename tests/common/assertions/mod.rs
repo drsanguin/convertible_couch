66b31341aa5a47672fe57bd78183b2db3fd6d8d5
drsanguin <thomas_perez@orange.fr>
2023-12-28 14:39:34 +0100

    test: better assertion messages

--- tests/common/assertions/mod.rs
+++ tests/common/assertions/mod.rs
@@ -6,7 +6,7 @@ pub fn assert_that_expected_monitor_has_been_set(
 ) {
     assert_eq!(
         actual_primary_monitor, expected_primary_monitor,
-        "Expected primary monitor to have been set to {} but it has been set to {}",
+        "Expected primary monitor to have been set to '{}' but it has been set to '{}'",
         expected_primary_monitor, actual_primary_monitor
     )
 }
@@ -14,7 +14,7 @@ pub fn assert_that_expected_monitor_has_been_set(
 pub fn fail_because_primary_monitor_has_not_changed(expected_primary_monitor: &str) {
     assert!(
         false,
-        "Expected primary monitor to have been set to {} but it has not been changed",
+        "Expected primary monitor to have been set to '{}' but it has not been changed",
         expected_primary_monitor
     )
 }
@@ -22,7 +22,7 @@ pub fn fail_because_primary_monitor_has_not_changed(expected_primary_monitor: &s
 pub fn fail_because_an_error_occured<T: Display>(error: T, expected_primary_monitor: &str) {
     assert!(
         false,
-        "Expected primary monitor to have been set to {} but if failed because of the error {}",
+        "Expected primary monitor to have been set to '{}' but if failed because of the error '{}'",
         expected_primary_monitor, error
     )
 }
