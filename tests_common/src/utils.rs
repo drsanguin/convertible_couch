8f5e4627b57d9a6ca691807bf9a9754565a6e837
drsanguin <thomas_perez@orange.fr>
2024-01-31 13:07:45 +0100

    refactor: utils module in tests_common

--- tests_common/src/utils.rs
+++ tests_common/src/utils.rs
@@ -1,7 +1,6 @@
 #[macro_export]
 macro_rules! func {
     () => {{
-        use convertible_couch_tests_common::fuzzing::Fuzzer;
         fn f() {}
         fn type_name_of<T>(_: T) -> &'static str {
             std::any::type_name::<T>()
@@ -17,10 +16,8 @@ pub fn encode_utf16<const T: usize>(string: &str) -> [u16; T] {
 
     string
         .encode_utf16()
-        .collect::<Vec<u16>>()
-        .iter()
         .enumerate()
-        .for_each(|(byte_index, byte)| bytes[byte_index] = *byte);
+        .for_each(|(byte_index, byte)| bytes[byte_index] = byte);
 
     bytes
 }
