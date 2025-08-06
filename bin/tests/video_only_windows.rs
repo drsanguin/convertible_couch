44c072b47da61bcaa6a6fc28baf4438abf76acc5
drsanguin <thomas_perez@orange.fr>
2025-08-06 13:39:02 +0200

    test: moved windows integration tests into a specific file

---
+++ bin/tests/video_only_windows.rs
@@ -0,0 +1,197 @@
+#![cfg(target_os = "windows")]
+
+use convertible_couch::{run_app, ApplicationResult, Arguments, Commands, SharedOpts, VideoOpts};
+use convertible_couch_lib::{
+    display_settings::{CurrentDisplaySettings, DisplaySettings, DisplaySettingsResult},
+    func,
+    log::LogLevel,
+    sound_settings::{CurrentSoundSettings, SoundSettings},
+    testing::fuzzing::Fuzzer,
+};
+use test_case::test_case;
+use windows::Win32::Graphics::Gdi::{
+    DISP_CHANGE, DISP_CHANGE_BADDUALVIEW, DISP_CHANGE_BADFLAGS, DISP_CHANGE_BADMODE,
+    DISP_CHANGE_BADPARAM, DISP_CHANGE_FAILED, DISP_CHANGE_NOTUPDATED, DISP_CHANGE_RESTART,
+};
+
+#[test_case(DISP_CHANGE_BADDUALVIEW => Err(String::from("The settings change was unsuccessful because the system is DualView capable.")); "when the error is BADDUALVIEW")]
+#[test_case(DISP_CHANGE_BADFLAGS => Err(String::from("An invalid set of flags was passed in.")); "when the error is DISP_CHANGE_BADFLAGS")]
+#[test_case(DISP_CHANGE_BADMODE => Err(String::from("The graphics mode is not supported.")); "when the error is DISP_CHANGE_BADMODE")]
+#[test_case(DISP_CHANGE_BADPARAM => Err(String::from("An invalid parameter was passed in. This can include an invalid flag or combination of flags.")); "when the error is DISP_CHANGE_BADPARAM")]
+#[test_case(DISP_CHANGE_FAILED => Err(String::from("The display driver failed the specified graphics mode.")); "when the error is DISP_CHANGE_FAILED")]
+#[test_case(DISP_CHANGE_NOTUPDATED => Err(String::from("Unable to write settings to the registry.")); "when the error is DISP_CHANGE_NOTUPDATED")]
+fn it_should_report_display_change_errors_that_happens_when_committing_changes(
+    disp_change: DISP_CHANGE,
+) -> Result<ApplicationResult, String> {
+    // Arrange
+    let mut fuzzer = Fuzzer::new(func!(), true);
+
+    let (primary_display_name, secondary_display_name) = fuzzer.generate_two_display_names();
+
+    let computer = fuzzer
+        .generate_computer()
+        .with_displays()
+        .of_which_there_are_at_least(2)
+        .whose_primary_is_named(primary_display_name.clone())
+        .with_a_secondary_named(secondary_display_name.clone())
+        .build_displays()
+        .for_which_committing_the_display_changes_fails_with(disp_change)
+        .build_computer();
+
+    let mut display_settings = CurrentDisplaySettings::new(computer.display_settings_api);
+    let mut sound_settings = CurrentSoundSettings::new(computer.audio_settings_api);
+
+    let args = Arguments {
+        command: Commands::VideoOnly {
+            video: VideoOpts {
+                desktop_display_name: primary_display_name.clone(),
+                couch_display_name: secondary_display_name.clone(),
+            },
+            shared: SharedOpts {
+                log_level: LogLevel::Off,
+            },
+        },
+    };
+
+    // Act
+    run_app(&args, &mut display_settings, &mut sound_settings)
+}
+
+#[test_case(DISP_CHANGE_BADDUALVIEW => Err(String::from("The settings change was unsuccessful because the system is DualView capable.")); "when the error is BADDUALVIEW")]
+#[test_case(DISP_CHANGE_BADFLAGS => Err(String::from("An invalid set of flags was passed in.")); "when the error is DISP_CHANGE_BADFLAGS")]
+#[test_case(DISP_CHANGE_BADMODE => Err(String::from("The graphics mode is not supported.")); "when the error is DISP_CHANGE_BADMODE")]
+#[test_case(DISP_CHANGE_BADPARAM => Err(String::from("An invalid parameter was passed in. This can include an invalid flag or combination of flags.")); "when the error is DISP_CHANGE_BADPARAM")]
+#[test_case(DISP_CHANGE_FAILED => Err(String::from("The display driver failed the specified graphics mode.")); "when the error is DISP_CHANGE_FAILED")]
+#[test_case(DISP_CHANGE_NOTUPDATED => Err(String::from("Unable to write settings to the registry.")); "when the error is DISP_CHANGE_NOTUPDATED")]
+fn it_should_report_display_change_errors_that_happens_for_some_displays(
+    disp_change: DISP_CHANGE,
+) -> Result<ApplicationResult, String> {
+    // Arrange
+    let mut fuzzer = Fuzzer::new(func!(), true);
+
+    let (primary_display_name, secondary_display_name) = fuzzer.generate_two_display_names();
+
+    let computer = fuzzer
+        .generate_computer()
+        .with_displays()
+        .of_which_there_are_at_least(2)
+        .whose_primary_is_named(primary_display_name.clone())
+        .with_a_secondary_named(secondary_display_name.clone())
+        .build_displays()
+        .for_which_changing_the_display_settings_fails_for_some_displays(disp_change)
+        .build_computer();
+
+    let mut display_settings = CurrentDisplaySettings::new(computer.display_settings_api);
+    let mut sound_settings = CurrentSoundSettings::new(computer.audio_settings_api);
+
+    let args = Arguments {
+        command: Commands::VideoOnly {
+            video: VideoOpts {
+                desktop_display_name: primary_display_name.clone(),
+                couch_display_name: secondary_display_name.clone(),
+            },
+            shared: SharedOpts {
+                log_level: LogLevel::Off,
+            },
+        },
+    };
+
+    // Act
+    run_app(&args, &mut display_settings, &mut sound_settings)
+}
+
+#[test]
+fn it_should_change_the_primary_display_of_computer_and_ask_for_reboot_when_required_after_committing_display_changes(
+) {
+    // Arrange
+    let mut fuzzer = Fuzzer::new(func!(), true);
+
+    let (primary_display_name, secondary_display_name) = fuzzer.generate_two_display_names();
+
+    let computer = fuzzer
+        .generate_computer()
+        .with_displays()
+        .of_which_there_are_at_least(2)
+        .whose_primary_is_named(primary_display_name.clone())
+        .with_a_secondary_named(secondary_display_name.clone())
+        .build_displays()
+        .for_which_committing_the_display_changes_fails_with(DISP_CHANGE_RESTART)
+        .build_computer();
+
+    let mut display_settings = CurrentDisplaySettings::new(computer.display_settings_api);
+    let mut sound_settings = CurrentSoundSettings::new(computer.audio_settings_api);
+
+    let args = Arguments {
+        command: Commands::VideoOnly {
+            video: VideoOpts {
+                desktop_display_name: primary_display_name.clone(),
+                couch_display_name: secondary_display_name.clone(),
+            },
+            shared: SharedOpts {
+                log_level: LogLevel::Off,
+            },
+        },
+    };
+
+    // Act
+    let actual_response = run_app(&args, &mut display_settings, &mut sound_settings);
+
+    // Assert
+    assert_eq!(
+        actual_response,
+        Ok(ApplicationResult::VideoOnly {
+            display_settings: DisplaySettingsResult {
+                new_primary: secondary_display_name,
+                reboot_required: true,
+            }
+        })
+    );
+}
+
+#[test]
+fn it_should_change_the_primary_display_of_computer_and_ask_for_reboot_when_required_after_changing_display_for_some_displays(
+) {
+    // Arrange
+    let mut fuzzer = Fuzzer::new(func!(), true);
+
+    let (primary_display_name, secondary_display_name) = fuzzer.generate_two_display_names();
+
+    let computer = fuzzer
+        .generate_computer()
+        .with_displays()
+        .of_which_there_are_at_least(2)
+        .whose_primary_is_named(primary_display_name.clone())
+        .with_a_secondary_named(secondary_display_name.clone())
+        .build_displays()
+        .for_which_changing_the_display_settings_fails_for_some_displays(DISP_CHANGE_RESTART)
+        .build_computer();
+
+    let mut display_settings = CurrentDisplaySettings::new(computer.display_settings_api);
+    let mut sound_settings = CurrentSoundSettings::new(computer.audio_settings_api);
+
+    let args = Arguments {
+        command: Commands::VideoOnly {
+            video: VideoOpts {
+                desktop_display_name: primary_display_name.clone(),
+                couch_display_name: secondary_display_name.clone(),
+            },
+            shared: SharedOpts {
+                log_level: LogLevel::Off,
+            },
+        },
+    };
+
+    // Act
+    let actual_response = run_app(&args, &mut display_settings, &mut sound_settings);
+
+    // Assert
+    assert_eq!(
+        actual_response,
+        Ok(ApplicationResult::VideoOnly {
+            display_settings: DisplaySettingsResult {
+                new_primary: secondary_display_name,
+                reboot_required: true,
+            }
+        })
+    );
+}
