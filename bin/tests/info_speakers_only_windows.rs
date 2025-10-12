fd4c6e7f17daa3abc4b107922f408a71978f83dd
drsanguin <thomas_perez@orange.fr>
2025-10-12 16:46:30 +0200

    test(info): speakers only windows

---
+++ bin/tests/info_speakers_only_windows.rs
@@ -0,0 +1,72 @@
+#![cfg(target_os = "windows")]
+
+use convertible_couch::testing::arrangements::{bootstrap_application, ArgumentsBuilder};
+use convertible_couch_lib::{
+    func,
+    testing::fuzzing::{ComputerBuilder, Fuzzer},
+    ApplicationError,
+};
+
+#[test]
+fn it_should_return_an_error_if_getting_the_speakers_count_fails() {
+    // Arrange
+    let mut fuzzer = Fuzzer::new(func!(), true);
+
+    let (default_speaker_name, alternative_speaker_name) = fuzzer.generate_two_speakers_names();
+
+    let computer = fuzzer
+        .generate_computer()
+        .with_speakers()
+        .of_which_there_are_at_least(2)
+        .whose_default_one_is_named(default_speaker_name.clone())
+        .with_an_alternative_one_named(alternative_speaker_name.clone())
+        .for_which_getting_the_speakers_count_fails()
+        .build_computer();
+
+    let mut application = bootstrap_application(computer);
+
+    let args = ArgumentsBuilder.info().speakers_only().build();
+
+    // Act
+    let actual_result = application.execute(&args);
+
+    // Assert
+    assert_eq!(
+        actual_result,
+        Err(ApplicationError::Custom(String::from(
+            "Failed to get the number of speakers"
+        )))
+    );
+}
+
+#[test]
+fn it_should_return_an_error_if_getting_the_speakers_fails() {
+    // Arrange
+    let mut fuzzer = Fuzzer::new(func!(), true);
+
+    let (default_speaker_name, alternative_speaker_name) = fuzzer.generate_two_speakers_names();
+
+    let computer = fuzzer
+        .generate_computer()
+        .with_speakers()
+        .of_which_there_are_at_least(2)
+        .whose_default_one_is_named(default_speaker_name.clone())
+        .with_an_alternative_one_named(alternative_speaker_name.clone())
+        .for_which_getting_the_speakers_fails()
+        .build_computer();
+
+    let mut application = bootstrap_application(computer);
+
+    let args = ArgumentsBuilder.info().speakers_only().build();
+
+    // Act
+    let actual_result = application.execute(&args);
+
+    // Assert
+    assert_eq!(
+        actual_result,
+        Err(ApplicationError::Custom(String::from(
+            "Failed to get the speakers"
+        )))
+    );
+}
