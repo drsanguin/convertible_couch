6a64413cca4e0bd8ce012be73bd6740ca166c9b2
drsanguin <thomas_perez@orange.fr>
2026-03-13 18:11:05 +0100

    docs: improve descriptions

--- bin/build.rs
+++ bin/build.rs
@@ -8,5 +8,6 @@ fn main() -> Result<()> {
     WindowsResource::new()
         .set_language(LANG_ENGLISH as u16)
         .set_icon("icon.ico")
+        .set("FileDescription", env!("CARGO_PKG_DESCRIPTION"))
         .compile()
 }
