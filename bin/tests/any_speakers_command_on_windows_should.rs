750e1dc0fe481059fee72dd27b5943a61c6df50d
drsanguin <thomas_perez@orange.fr>
2026-04-06 18:01:29 +0200

    refactor: speakers error tests

---
+++ bin/tests/any_speakers_command_on_windows_should.rs
@@ -0,0 +1,70 @@
+#![cfg(target_os = "windows")]
+
+use convertible_couch_lib::func;
+use convertible_couch_testing::{
+    arrangements::{
+        builders::{
+            application::ApplicationBuilder,
+            arguments::{ArgumentsBuilder, SpeakersCommand},
+        },
+        fuzzing::{ComputerBuilder, Fuzzer, speakers::Function},
+    },
+    assertions::assert_that_result_is_a_win32_error,
+};
+use test_case::test_matrix;
+
+#[test_matrix(
+    [
+        SpeakersCommand::ChangeDisplaysAndSpeakers,
+        SpeakersCommand::ChangeSpeakers,
+        SpeakersCommand::InfoDisplaysAndSpeakers,
+        SpeakersCommand::InfoSpeakers
+    ], [
+        Function::CoInitializeEx,
+        Function::CoCreateIMMDeviceEnumerator,
+        Function::IMMDeviceEnumeratorEnumAudioEndpoints,
+        Function::IMMDeviceGetId,
+        Function::IMMDeviceCollectionGetCount,
+        Function::IMMDeviceCollectionItem,
+        Function::IMMDeviceOpenPropertyStore,
+        Function::PropertyStoreGetValue,
+    ]; "when"
+)]
+fn report_any_speakers_error(speakers_command: SpeakersCommand, function: Function) {
+    // Arrange
+    let mut fuzzer = Fuzzer::new(func!(), true);
+
+    let (primary_display_name, secondary_display_name) = fuzzer.generate_two_display_names();
+    let (default_speaker_name, alternative_speaker_name) = fuzzer.generate_two_speakers_names();
+    let win_32_error = fuzzer.generate_win_32_error(&[]);
+
+    let computer = fuzzer
+        .generate_computer()
+        .with_displays()
+        .of_which_there_are_at_least(2)
+        .whose_primary_is_named(&primary_display_name)
+        .with_a_secondary_named(&secondary_display_name)
+        .build_displays()
+        .with_speakers()
+        .of_which_there_are_at_least(2)
+        .whose_default_one_is_named(&default_speaker_name)
+        .with_an_alternative_one_named(&alternative_speaker_name)
+        .for_which_function_fails_with(function, win_32_error)
+        .build_computer();
+
+    let mut application = ApplicationBuilder::new(computer).build();
+
+    let args = ArgumentsBuilder::speakers_command(
+        speakers_command,
+        &primary_display_name,
+        &secondary_display_name,
+        &default_speaker_name,
+        &alternative_speaker_name,
+    );
+
+    // Act
+    let actual_result = application.execute(&args);
+
+    // Assert
+    assert_that_result_is_a_win32_error(actual_result, win_32_error);
+}
--- testing/src/arrangements/fuzzing/speakers/settings_api/behaviour/windows.rs
+++ testing/src/arrangements/fuzzing/speakers/settings_api/behaviour/windows.rs
@@ -1,12 +1,20 @@
+use windows::Win32::Foundation::WIN32_ERROR;
+
 use crate::arrangements::fuzzing::speakers::settings_api::behaviour::FuzzedSpeakersSettingsApiBehaviour;
 
 #[derive(Clone, Default)]
 pub struct FuzzedWindowsSpeakersSettingsApiBehaviour {
-    pub getting_the_speakers_count_fails: bool,
-    pub getting_the_speakers_fails: bool,
-    pub getting_the_default_speaker_fails: bool,
-    pub setting_the_default_speaker_fails: bool,
-    pub initializing_the_com_library_fails: bool,
+    pub co_initialize_ex_error: Option<WIN32_ERROR>,
+    pub co_create_immdevice_enumerator_error: Option<WIN32_ERROR>,
+    pub immdevice_enumerator_get_default_audio_endpoint_error: Option<WIN32_ERROR>,
+    pub immdevice_enumerator_enum_audio_endpoints_error: Option<WIN32_ERROR>,
+    pub immdevice_get_id_error: Option<WIN32_ERROR>,
+    pub immdevice_collection_get_count_error: Option<WIN32_ERROR>,
+    pub immdevice_collection_item_error: Option<WIN32_ERROR>,
+    pub immdevice_open_property_store_error: Option<WIN32_ERROR>,
+    pub property_store_get_value_error: Option<WIN32_ERROR>,
+    pub co_create_ipolicy_config_vista_error: Option<WIN32_ERROR>,
+    pub ipolicy_config_vista_set_default_endpoint_error: Option<WIN32_ERROR>,
 }
 
 impl FuzzedSpeakersSettingsApiBehaviour for FuzzedWindowsSpeakersSettingsApiBehaviour {}
